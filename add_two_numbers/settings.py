1c1dd71be9951433c50c73d12cc30d6f1f806230
injusticescorpio <arjunvishnu2000@gmail.com>
2020-11-27 21:24:15 +0530

    updated

--- add_two_numbers/settings.py
+++ add_two_numbers/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR=os.path.join(BASE_DIR,"firstapp/templates")
+TEMPLATE_DIR=os.path.join(BASE_DIR,"firstapp/templates") # for templates
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
